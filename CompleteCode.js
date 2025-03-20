104009d15e383eccbd94f5616b830935fbf879c0
Govind244 <govindsaraswat244@gmail.com>
2025-03-20 21:59:48 +0530

    Wages with conditions added

--- CompleteCode.js
+++ CompleteCode.js
@@ -83,3 +83,20 @@ for(let i = 1; i<=numberOfWorkingDays;i++){
 
 let empWage = empHrs*wagePerHour;
 console.log("Monthly Wage of the Employee for hours "+empHrs+" is "+empWage);
+
+
+
+//UC-5 calculating Employee wage Untill hours are 160 or days completed are 20 
+
+empHrs = 0;
+let numberOfTotalDays = 0;
+while(empHrs <= 160 && numberOfTotalDays < 20){
+    
+    empCheck = Math.floor(Math.random()*10)%3;
+    empHrs += getWorkingHours(empCheck);
+    numberOfTotalDays++;
+
+}
+empWage = empHrs*wagePerHour; 
+
+console.log("Total working days are : "+numberOfTotalDays+", Total working hours are : "+empHrs+", Wage of Employee is : "+empWage);
